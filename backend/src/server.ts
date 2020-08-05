13c72aaf99b0019be5900930e8c1df03e47d93ea
Vitor Bertti <berttivitor@gmail.com>
2020-08-04 21:03:13 -0300

    Created index and store functions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,8 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (req, res) => {
-   return res.send('opa');
-});
+app.use(routes);
 
 app.listen(3333);
